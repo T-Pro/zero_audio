e3ce6f3f09799910222df4719517a4686883192c
Pedro Paulo de Amorim <pp.amorim@hotmail.com>
2020-11-18 20:39:24 +0000

    Add data counter

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,37 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     println!("Starting to confuse the audio...");
 
-    let paths = std::fs::read_dir("./input")?;
+    let raw_paths: std::fs::ReadDir = std::fs::read_dir("./input")?;
+
+    let mut filtered_paths = raw_paths.filter_map(|entry| {
+        let path: std::path::PathBuf = entry.ok()?.path();
+        match path.extension().or(None)?.to_str().unwrap() {
+            "caf" | "caff" | "wav" | "pcm" | "raw" => Some(path),
+            _ => None,
+        }
+    });
+
+    let paths = filtered_paths.collect::<Vec<std::path::PathBuf>>();
+
+    if paths.is_empty() {
+        println!("No input caf, caff, wav, pcm or raw files found");
+        return Ok(())
+    }
 
     for path in paths {
 
-        let path: std::path::PathBuf = path.unwrap().path();
         let path_c = path.clone();
         let path_str = path_c.display();
         println!("Processing: {}", path_str);
 
+        let header_size = match path_c.extension().unwrap().to_str().unwrap() {
+            "caf" | "caff" => 4096,
+            "wav" => 44,
+            _ => 0,
+        };
+
         let file_len = std::fs::metadata(&path).unwrap().len();
-        let file_len_no_header = file_len - 4096;
+        let file_len_no_header = file_len - header_size;
 
         let output = format!("./output/{}", path.file_name().unwrap().to_str().unwrap());
 
@@ -31,7 +51,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         output.write(&buffer[..read]);
 
-        for i in 0..file_len_no_header {
+        for _ in 0..file_len_no_header {
             output.write(&[0]);
         }
 
